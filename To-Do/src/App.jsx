9854d60119320597e04f0303beef353546643fe2
sankeerthankj <sankeerthan.kj@gmail.com>
2024-12-10 16:50:25 +0530

    Adding To-Do Code

---
+++ To-Do/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+import './App.css'
+import { TodoWrapper } from './Components/TodoWrapper'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <TodoWrapper/>
+    </>
+  )
+}
+
+export default App
